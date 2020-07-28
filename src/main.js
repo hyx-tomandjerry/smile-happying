22c6e8491684bd4122fd6eac4c016dc5fa65c6b5
胡玉霞 <huyx@xiangmulin.cn>
2020-07-28 19:07:35 +0800

    '123'

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/normalize.css'
+import './assets/css/main.css'
+import '../static/iconfont/icon.css'
 Vue.config.productionTip = false
 
 //按需引入
-import {Button,Row,Col} from "vant";
-Vue.use(Button).use(Row).use(Col);
+import {Button,Row,Col,Swipe,SwipeItem} from "vant";
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
